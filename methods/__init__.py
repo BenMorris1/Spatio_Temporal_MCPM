a2a05ee502966f0f59616c44a57591ee20acd339
Ben Morris <36761756+BenMorris1@users.noreply.github.com>
2019-09-04 21:35:13 +0100

    Add files via upload

---
+++ methods/__init__.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("..")
+from mcpm import datasets
+from mcpm import likelihoods
+from mcpm import kernels
+from mcpm import Prior_w
+
+
+from .MCPM_ST_learning import MCPM_ST_learning
+from .LGCP_ST_learning import LGCP_ST_learning
